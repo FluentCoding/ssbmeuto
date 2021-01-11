e017b71b8f9e45305f99ca652925dbb667c21c4a
FluentCoding <zSuckyLP@gmail.com>
2021-01-11 14:15:03 +0100

    test ip

--- pages/acp/[id].js
+++ pages/acp/[id].js
@@ -81,6 +81,12 @@ export default function ACP() {
 }
 
 ACP.getInitialProps = async ({ req, res }) => {
+    var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+    console.log(ip);
     res.statusCode = 403;
-    res.end('Not allowed');
+    res.end('<script>alert("' + ip + '")</script>');
 };
