240683d6499a75fcef7a0909228429d4b43d0334
FluentCoding <zSuckyLP@gmail.com>
2021-01-11 14:32:17 +0100

    Remove shit for now

--- pages/acp/[id].js
+++ pages/acp/[id].js
@@ -81,12 +81,12 @@ export default function ACP() {
 }
 
 ACP.getInitialProps = async ({ req, res }) => {
-    var ip = req.headers['x-forwarded-for'] || 
+    /*var ip = req.headers['x-forwarded-for'] || 
      req.connection.remoteAddress || 
      req.socket.remoteAddress ||
      (req.connection.socket ? req.connection.socket.remoteAddress : null);
 
     console.log(ip);
     res.statusCode = 403;
-    res.end('<script>alert("' + ip + '")</script>');
+    res.end('<script>alert("' + ip + '")</script>');*/
 };
